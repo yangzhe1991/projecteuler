4b0c99be18aa31183293cd999ec9773f61cf0fac
Philo Yang <ud1937@gmail.com>
2012-09-20 21:19:36 +0800

    p3

--- scheme/p3.scm
+++ scheme/p3.scm
@@ -1,2 +1,9 @@
 (load "util.ss")
-
+(define (p3 x)
+  (let loop ((i 2) (ans 0))
+    (if (> i (sqrt x))
+        ans
+        (if (and (= 0 (modulo x i))(prime? i))
+            (loop (+ i 1) i)
+            (loop (+ i 1) ans)))))
+(p3 600851475143)
---
+++ scheme/p4.scm
@@ -0,0 +1,2 @@
+(define (palin x)
+  
